51cf2dd1d80ac4d5bd659e6880513ab06622caf4
Arshiya143 <arshiya.patel1999@gmail.com>
2024-06-18 14:33:56 +0530

    Changed Panel

--- position.js
+++ position.js
@@ -2,34 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const draggable = document.getElementById("draggable");
   const progress = document.getElementById("progress");
   const container = document.getElementById("gridContainer");
+  const tableContainer = document.getElementById("tableContainer");
+  const zoom = document.getElementById("zoom");
+  const detailContent = document.getElementById("detailContent");
+  const previewContent = document.getElementById("previewContent");
+  const panel = document.getElementById("panel");
+  const gridContainer = document.getElementById("gridContainer");
+  const resizer = document.querySelector(".resizer");
+  const sidebar = document.querySelector(".resizable-sidebar");
 
-  function updateGridColumns(percentage) {
-    let columns;
-    if (percentage < 0) {
-      columns = 12;
-    } else if (percentage < 16) {
-      columns = 11;
-    } else if (percentage < 32) {
-      columns = 10;
-    } else if (percentage < 48) {
-      columns = 9;
-    } else if (percentage < 64) {
-      columns = 8;
-    } else if (percentage < 80) {
-      columns = 7;
-    } else if (percentage < 96) {
-      columns = 6;
-    } else {
-      columns = 5;
-    }
+  const updateGridColumns = (percentage) => {
+    const columns = Math.max(5, 12 - Math.floor(percentage / 16));
     container.className = `grid grid-cols-${columns} p-4 transition-all duration-300 dive-height`;
     localStorage.setItem("gridPercentage", percentage); // Save percentage to local storage
-  }
+  };
+
+  const setDraggablePosition = (percentage) => {
+    const y = (percentage / 100) * 150;
+    progress.style.height = percentage + "%";
+    draggable.style.top = y + "px";
+  };
 
   draggable.addEventListener("mousedown", function (event) {
     event.preventDefault();
     const slider = draggable.parentElement;
     const sliderRect = slider.getBoundingClientRect();
+
     const onMouseMove = (e) => {
       let y = e.clientY - sliderRect.top;
       y = Math.max(0, Math.min(y, sliderRect.height));
@@ -39,126 +37,145 @@ document.addEventListener("DOMContentLoaded", function () {
       updateGridColumns(percentage);
     };
 
+    const onMouseUp = () => {
+      document.removeEventListener("mousemove", onMouseMove);
+    };
+
     document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener(
-      "mouseup",
-      () => {
-        document.removeEventListener("mousemove", onMouseMove);
-      },
-      { once: true }
-    );
+    document.addEventListener("mouseup", onMouseUp, { once: true });
   });
 
-  // Function to set draggable position and progress based on percentage
-  function setDraggablePosition(percentage) {
-    const slider = draggable.parentElement;
-    const sliderRect = slider.getBoundingClientRect();
-    const y = (percentage / 100) * sliderRect.height;
-    draggable.style.top = y + "px";
-    progress.style.height = percentage + "%";
-  }
-
-  // Retrieve percentage from local storage and update grid and draggable position
-  const savedPercentage = localStorage.getItem("gridPercentage");
-  if (savedPercentage !== null) {
-    const percentage = parseFloat(savedPercentage);
-    setDraggablePosition(percentage);
-    updateGridColumns(percentage);
-  }
-});
-function toggleView() {
-  const gridContainer = document.getElementById("gridContainer");
-  const tableContainer = document.getElementById("tableContainer");
-  const zoom = document.getElementById("zoom");
-
-  if (gridContainer.classList.contains("hidden")) {
-    gridContainer.classList.remove("hidden");
-    tableContainer.classList.add("hidden");
-    zoom.classList.remove("hidden");
-  } else {
-    gridContainer.classList.add("hidden");
-    tableContainer.classList.remove("hidden");
-    zoom.classList.add("hidden");
-  }
-}
-
-// Attach the toggle function to the "View" button
-document.querySelector(".view-button").addEventListener("click", toggleView);
-
-function toggleDropdown(id) {
-  const element = document.getElementById(id);
-  element.classList.toggle("hidden");
-}
-
-function generateRandomPassword() {
-  const passwordField = document.getElementById("password");
-  passwordField.value = Math.random().toString(36).slice(-8);
-}
-
-function togglePopup(popupId, event) {
-  var popup = document.getElementById(popupId);
-  if (popup) {
-    popup.classList.toggle("hidden");
-  } else {
-    console.error("Popup with id " + popupId + " not found.");
-  }
-}
-document.addEventListener("DOMContentLoaded", function () {
-  // Toggle view functionality
-  function toggleView(view) {
-    var detailContent = document.getElementById("detailContent");
-    var previewContent = document.getElementById("previewContent");
-    var panel = document.getElementById("panel");
-    var gridContainer = document.getElementById("gridContainer");
-
-    panel.classList.toggle("hidden");
-
-    if (!panel.classList.contains("hidden")) {
-      gridContainer.style.marginRight = panel.offsetWidth + "px";
-    } else {
-      gridContainer.style.marginRight = "0";
+  // Initial setup on page load
+  window.addEventListener("load", () => {
+    const savedPercentage = localStorage.getItem("gridPercentage");
+    if (savedPercentage !== null) {
+      const percentage = parseFloat(savedPercentage);
+      setDraggablePosition(percentage);
+      updateGridColumns(percentage); // Ensure grid columns are updated initially
     }
+  });
+
+  const toggleView = () => {
+    gridContainer.classList.toggle("hidden");
+    tableContainer.classList.toggle("hidden");
+    zoom.classList.toggle("hidden");
+  };
+
+  const togglePopup = (popupId) => {
+    const popup = document.getElementById(popupId);
+    if (popup) {
+      popup.classList.toggle("hidden");
+    } else {
+      console.error("Popup with id " + popupId + " not found.");
+    }
+  };
+
+  const togglePanel = (view) => {
+    const isPanelHidden = panel.classList.contains("hidden");
 
     if (view === "detail") {
-      detailContent.classList.remove("hidden");
-      previewContent.classList.add("hidden");
+      if (isPanelHidden || detailContent.classList.contains("hidden")) {
+        panel.classList.remove("hidden");
+        detailContent.classList.remove("hidden");
+        previewContent.classList.add("hidden");
+        gridContainer.style.marginRight =
+          tableContainer.style.marginRight = `${panel.offsetWidth}px`;
+      } else {
+        panel.classList.add("hidden");
+        gridContainer.style.marginRight = tableContainer.style.marginRight =
+          "0";
+      }
     } else if (view === "preview") {
-      detailContent.classList.add("hidden");
-      previewContent.classList.remove("hidden");
+      if (isPanelHidden || previewContent.classList.contains("hidden")) {
+        panel.classList.remove("hidden");
+        previewContent.classList.remove("hidden");
+        detailContent.classList.add("hidden");
+        gridContainer.style.marginRight =
+          tableContainer.style.marginRight = `${panel.offsetWidth}px`;
+      } else {
+        panel.classList.add("hidden");
+        gridContainer.style.marginRight = tableContainer.style.marginRight =
+          "0";
+      }
     }
-  }
+  };
 
-  // Resize sidebar functionality
-  const resizer = document.querySelector(".resizer");
-  const sidebar = document.querySelector(".resizable-sidebar");
+  const initResize = (e) => {
+    window.addEventListener("mousemove", startResizing);
+    window.addEventListener("mouseup", stopResizing);
+  };
+
+  const startResizing = (e) => {
+    const minWidth = 288; // Set your minimum width here
+    const maxWidth = 600; // Set your maximum width here
+    let newWidth = window.innerWidth - e.clientX;
+
+    // Ensure the new width is within the specified range
+    if (newWidth < minWidth) {
+      newWidth = minWidth;
+    } else if (newWidth > maxWidth) {
+      newWidth = maxWidth;
+    }
+
+    sidebar.style.width = `${newWidth}px`;
+
+    const margin = !panel.classList.contains("hidden") ? `${newWidth}px` : "0";
+    document.getElementById("gridContainer").style.marginRight = margin;
+    document.getElementById("tableContainer").style.marginRight = margin;
+  };
+
+  const stopResizing = () => {
+    window.removeEventListener("mousemove", startResizing);
+    window.removeEventListener("mouseup", stopResizing);
+  };
 
   resizer.addEventListener("mousedown", initResize);
 
-  function initResize(e) {
-    window.addEventListener("mousemove", startResizing);
-    window.addEventListener("mouseup", stopResizing);
-  }
+  // const ContextMenu = document.getElementById("context-menu");
+  // const group = document.querySelector(".group");
 
-  function startResizing(e) {
-    let newWidth = window.innerWidth - e.clientX;
-    sidebar.style.width = `${newWidth}px`;
-    let panel = document.getElementById("panel");
+  // // Function to position a menu and make it visible
+  // function positionAndShowMenu(menu, event, container) {
+  //   const menuWidth = menu.offsetWidth;
+  //   const menuHeight = menu.offsetHeight;
+  //   const containerRect = container.getBoundingClientRect();
+  //   let top = event.clientY;
+  //   let left = event.clientX;
 
-    // Update the grid container's margin based on panel visibility
-    if (!panel.classList.contains("hidden")) {
-      document.getElementById(
-        "gridContainer"
-      ).style.marginRight = `${newWidth}px`;
-    } else {
-      document.getElementById("gridContainer").style.marginRight = "0";
-    }
-  }
+  //   if (event.clientY + menuHeight > containerRect.bottom) {
+  //     top = containerRect.bottom - menuHeight;
+  //   }
+  //   if (event.clientX + menuWidth > containerRect.right) {
+  //     left = containerRect.right - menuWidth;
+  //   }
 
-  function stopResizing() {
-    window.removeEventListener("mousemove", startResizing);
-    window.removeEventListener("mouseup", stopResizing);
-  }
+  //   if (top < containerRect.top) {
+  //     top = containerRect.top;
+  //   }
+  //   if (left < containerRect.left) {
+  //     left = containerRect.left;
+  //   }
 
-  // Expose toggleView to global scope so it can be called from HTML
+  //   menu.style.top = `${top}px`;
+  //   menu.style.left = `${left}px`;
+  //   menu.classList.remove("hidden");
+  // }
+
+  // // Right-click on Dashboard to show Dashboard Context Menu
+  // group.addEventListener("contextmenu", (event) => {
+  //   const target = event.target;
+  //   if (!target.classList.contains("app") && !target.closest(".app")) {
+  //     event.preventDefault();
+  //     positionAndShowMenu(ContextMenu, event, group);
+  //   }
+  // });
+
+  // // Click anywhere to hide menus
+  // document.addEventListener("click", () => {
+  //   ContextMenu.classList.add("hidden");
+  // });
+
+  window.togglePanel = togglePanel;
+  window.togglePopup = togglePopup;
   window.toggleView = toggleView;
 });
